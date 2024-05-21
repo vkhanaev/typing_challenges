2cd67b835c80254ff51c9b8497b3f1a32d122d14
Valentin Khanaev <vkhanaev@ya.ru>
2024-05-21 12:58:32 +0300

    добавлены аннотации level_3

--- level_3/2.py
+++ level_3/2.py
@@ -1,7 +1,9 @@
+import typing
+
 from constants import ___
 
 
-def calculate_total_spent_for_user(user: ___) -> ___:
+def calculate_total_spent_for_user(user: typing.TypedDict[str, str | int | list[int]]) -> int:
     # попробуй тут воспользовать typing.TypedDict
     pass
 
