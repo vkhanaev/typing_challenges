9e6ccb8d6ad69562bc1f974b7441adf66f47b045
Valentin Khanaev <vkhanaev@ya.ru>
2024-05-26 16:37:44 +0300

    исправил 8.py

--- level_1/8.py
+++ level_1/8.py
@@ -1,12 +1,10 @@
 import decimal
 import uuid
-from typing import Callable
 
 from constants import ___
 
 
 def get_user_balance(user_id: uuid.UUID) -> decimal.Decimal:
-    #reveal_type(user_id)
     pass
 
 
